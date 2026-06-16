177c54319add8659960349ee6141635e91b55c53
Emanuele Zeppa <zeppaemanuele@gmail.com>
2026-06-16 17:56:31 +0200

    Parte relativa Homepage, base aggiunta della sezione relativa con strutturazione della pagina.

--- Clembry_front/src/app/features/homepage/homepage.ts
+++ Clembry_front/src/app/features/homepage/homepage.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../navbar/navbar';
-import { Footer } from '../footer/footer';
-import { Hero } from '../hero/hero';
-import { Services } from '../services/services';
-import { Courses } from "../courses/courses";
-
 
 @Component({
-  selector: 'app-homepage',
-  imports: [  Hero, Services],
-  templateUrl: './homepage.html',
-  styleUrl: './homepage.css',
+selector: 'app-homepage', // nome del componente usato nel template
+templateUrl: './homepage.html', // collega il file HTML
+styleUrls: ['./homepage.css'] // collega il file CSS
 })
-export class Homepage {}
+export class Homepage {
+// proprietà per gestire eventuali pulsanti o messaggi
+titolo: string = 'Trova il docente per le tue ripetizioni online';
+sottotitolo: string = 'Prenota lezioni individuali o di gruppo, accedi ai materiali e monitora i tuoi progressi.';
+
+// metodo per pulsante (esempio)
+cercaDocente() {
+alert('Funzione di ricerca docente');
+}
+}
