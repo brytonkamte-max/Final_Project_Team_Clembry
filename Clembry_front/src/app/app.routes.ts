6e99a7ea026be9146d5bf157d7380e4e0f83f9f7
clark1492 <76492078+clark1492@users.noreply.github.com>
2026-06-17 15:59:12 +0200

    Aggiunte pagine personali docente e studente

--- Clembry_front/src/app/app.routes.ts
+++ Clembry_front/src/app/app.routes.ts
@@ -5,17 +5,21 @@ import { Registration } from './features/registration/registration';
 import { Teachers } from './features/teachers/teachers';
 import { Courses } from './features/courses/courses';
 import { ContactsComponent } from './features/contacts-component/contacts-component';
+import { userGuard } from './core/guards/user-guard';
+import { teacherGuard } from './core/guards/teacher-guard';
+import { PersonalArea } from './features/personal-area/personal-area';
+import { TeacherPersonalArea } from './features/teacher-personal-area/teacher-personal-area';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Homepage },
   { path: 'courses', component: Courses },
-  { path: 'teachers', component: Teachers },
+  { path: 'teachers', component: Teachers ,canActivate: [userGuard] },
   // { path: 'howitworks', component: Homepage },
-   { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts', component: ContactsComponent },
   // { path: 'about', component: Homepage },
   { path: 'login', component: Login },
   { path: 'registration', component: Registration },
-  { path: 'personalArea', component: Homepage },
-  { path: 'teacherPersonalArea', component: Homepage },
+  { path: 'personalArea', component: PersonalArea , canActivate: [userGuard] },
+  { path: 'teacherPersonalArea', component: TeacherPersonalArea ,canActivate: [teacherGuard] },
 ];
